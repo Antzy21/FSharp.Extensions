5ff3bd47c21ba7478580262eb6b78d197890f4d1
Anthony Dunford <Anthony@Dunford.uk.com>
2023-03-29 06:14:47 +0100

    Add "toBlock" function for String module

--- String.fs
+++ String.fs
@@ -5,3 +5,13 @@ open System
 /// Concatenate a sequence of chars into a string
 let ofSeq (charSeq: seq<char>) : string =
     charSeq |> String.Concat
+
+/// Takes a string and wraps it onto new lines such that the number of lines equals the length of each line
+let toBlock (str: string) : string =
+    let size = int(Math.Sqrt(str.Length))
+    str
+    |> Seq.rev
+    |> Seq.chunkBySize size
+    |> Seq.fold (fun s chunk ->
+        s + (String.Concat chunk) + "\n"
+    ) ""
