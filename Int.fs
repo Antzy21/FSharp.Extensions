29072031096d605cff6a02b18a103bb489e8b4fe
Anthony Dunford <Anthony@Dunford.uk.com>
2023-04-03 03:42:31 +0100

    Adds a summary of the int parser option function

--- Int.fs
+++ Int.fs
@@ -1,7 +1,6 @@
 ﻿module FSharp.Extensions.Int
 
-open System
-
+/// Converts the string representation of a number to its 32-bit signed integer equivalent. The return value is an optional int.
 let tryParse (str:string) =
   match System.Int32.TryParse str with
   | true, int -> Some int
