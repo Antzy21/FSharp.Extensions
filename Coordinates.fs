f3bf3c60b4d22020338565f5c7a98c5a5f9f6233
Anthony Dunford <Anthony@Dunford.uk.com>
2023-04-03 03:14:41 +0100

    rename getDistanceBetweenCoordinates to "Shift" and add actual distance function

--- Coordinates.fs
+++ Coordinates.fs
@@ -8,9 +8,14 @@ type coordinates<'Size when 'Size :> INumber<'Size>> = 'Size * 'Size
 ///<summary>Contains operations for working with values of type <see cref="T:FSharp.Extensions.coordinates" />.</summary>
 module Coordinates =
     
-    /// Get the metropolitan distance between two given coordinates
-    let getDistanceBetweenCoordinates ((x1,y1): coordinates<'Size>) ((x2,y2): coordinates<'Size>) : coordinates<'Size> =
+    /// Get the shift between two given coordinates
+    let getShiftBetweenCoordinates ((x1,y1): coordinates<'Size>) ((x2,y2): coordinates<'Size>) : coordinates<'Size> =
         (x2-x1,y2-y1)
+
+    /// Get the metropolitan distance between two given coordinates
+    let getDistanceBetweenCoordinates ((x1,y1): coordinates<'Size>) ((x2,y2): coordinates<'Size>) : 'Size =
+        getShiftBetweenCoordinates (x1,y1) (x2, y2)
+        |> (fun (x,y) -> x+y)
 
     /// Get new coordinates after shifting given coordinates by a given shift.
     /// Starting coordinates and shift parameters are interchangable thanks to the communitivity property of addition!
