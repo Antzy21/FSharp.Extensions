6d39a8de2c543a48b3e52fba59ca48982fd998a4
Anthony Dunford <Anthony@Dunford.uk.com>
2023-05-10 18:18:03 +0100

    Alter Coordinates module to work with struct type

--- FSharp.Extensions/Coordinates.fs
+++ FSharp.Extensions/Coordinates.fs
@@ -15,28 +15,28 @@ module Coordinates =
     /// Get the metropolitan distance between two given coordinates
     let getDistanceBetweenCoordinates ((x1,y1): coordinates<'Size>) ((x2,y2): coordinates<'Size>) : 'Size =
         getShiftBetweenCoordinates (x1,y1) (x2, y2)
-        |> (fun (x,y) -> 'Size.Abs(x) + 'Size.Abs(y))
+        |> (fun struct (x,y) -> 'Size.Abs(x) + 'Size.Abs(y))
 
     /// Get new coordinates after shifting given coordinates by a given shift.
     /// Starting coordinates and shift parameters are interchangable thanks to the communitivity property of addition!
-    let getAfterShift ((i, j): 'Size * 'Size) ((x, y): coordinates<'Size>) : coordinates<'Size> =
-        (x+i, y+j)
+    let getAfterShift ((i, j): struct ('Size * 'Size)) ((x, y): coordinates<'Size>) : coordinates<'Size> =
+        struct (x+i, y+j)
     
     /// Get a sequence of coordinates after applying shifts to given coordinates.
-    let getAfterShifts (start: coordinates<'Size>) (shifts: ('Size * 'Size) seq) : coordinates<'Size>seq =
+    let getAfterShifts (start: coordinates<'Size>) (shifts: (struct ('Size * 'Size)) seq) : coordinates<'Size>seq =
         Seq.map (getAfterShift start) shifts
     
     /// Get up to 8 unique coordinates that are a given metropolitan distance away from given coordinates
-    let getAfterShiftInAllDirections (start: coordinates<'Size>) ((i, j) : 'Size * 'Size) : coordinates<'Size>seq =
+    let getAfterShiftInAllDirections (start: coordinates<'Size>) ((i, j) : struct ('Size * 'Size)) : coordinates<'Size>seq =
         [
-            (i,j);
-            (i,-j);
-            (-i,j);
-            (-i,-j)
-            (j,i);
-            (j,-i);
-            (-j,i);
-            (-j,-i)
+            struct (i,j);
+            struct (i,-j);
+            struct (-i,j);
+            struct (-i,-j)
+            struct (j,i);
+            struct (j,-i);
+            struct (-j,i);
+            struct (-j,-i)
         ]
         |> Seq.distinct
         |> getAfterShifts start
@@ -44,20 +44,20 @@ module Coordinates =
     /// Get 4 coordinates that are orthogonal (share a side with) some given coordinates
     let getOrthogonalAdjacentCoordinates (start: coordinates<'Size>) : coordinates<'Size>seq =
         [
-            ('Size.Zero,'Size.One);
-            ('Size.One,'Size.Zero);
-            ('Size.Zero,-'Size.One);
-            (-'Size.One,'Size.Zero)
+            struct ('Size.Zero,'Size.One);
+            struct ('Size.One,'Size.Zero);
+            struct ('Size.Zero,-'Size.One);
+            struct (-'Size.One,'Size.Zero)
         ]
         |> getAfterShifts start
 
     /// Get 4 coordinates at diagonals to some given coordinates
     let getDiagonallyAdjacentCoordinates (start: coordinates<'Size>) : coordinates<'Size>seq =
         [
-            ('Size.One,'Size.One);
-            ('Size.One,-'Size.One);
-            (-'Size.One,-'Size.One);
-            (-'Size.One,'Size.One)
+            struct ('Size.One,'Size.One);
+            struct ('Size.One,-'Size.One);
+            struct (-'Size.One,-'Size.One);
+            struct (-'Size.One,'Size.One)
         ]
         |> getAfterShifts start
 
@@ -67,7 +67,7 @@ module Coordinates =
         |> Seq.append (getOrthogonalAdjacentCoordinates start)
         
     /// Get all coordinates after repeating a shift, up to and including when a stopping function criteria is met.
-    let rec afterRepeatedShift (stopAt: coordinates<'Size> -> bool) (shift: 'Size * 'Size) (start: coordinates<'Size>) : coordinates<'Size>list =
+    let rec afterRepeatedShift (stopAt: coordinates<'Size> -> bool) (shift: struct ('Size * 'Size)) (start: coordinates<'Size>) : coordinates<'Size>list =
         let newCoordinates = getAfterShift shift start
         if stopAt newCoordinates then
             [newCoordinates]
