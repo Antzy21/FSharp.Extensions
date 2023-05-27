c212829c9ac2dc085297fa6251a2b92bf4ff670c
Anthony Dunford <trashzy21@gmail.com>
2023-05-27 11:22:07 +0100

    Make shift type

--- FSharp.Extensions/Coordinates.fs
+++ FSharp.Extensions/Coordinates.fs
@@ -5,6 +5,8 @@ open System.Numerics
 /// A tuple of two INumbers, used for referencing a 2D discrete grid.
 type coordinates<'Size when 'Size :> INumber<'Size>> = (struct ('Size * 'Size))
 
+type shift<'Size when 'Size :> INumber<'Size>> = (struct ('Size * 'Size))
+
 ///<summary>Contains operations for working with values of type <see cref="T:FSharp.Extensions.coordinates" />.</summary>
 module Coordinates =
     
@@ -19,15 +21,15 @@ module Coordinates =
 
     /// Get new coordinates after shifting given coordinates by a given shift.
     /// Starting coordinates and shift parameters are interchangable thanks to the communitivity property of addition!
-    let getAfterShift ((i, j): struct ('Size * 'Size)) ((x, y): coordinates<'Size>) : coordinates<'Size> =
+    let getAfterShift ((i, j): shift<'Size>) ((x, y): coordinates<'Size>) : coordinates<'Size> =
         struct (x+i, y+j)
     
     /// Get a sequence of coordinates after applying shifts to given coordinates.
-    let getAfterShifts (start: coordinates<'Size>) (shifts: (struct ('Size * 'Size)) seq) : coordinates<'Size>seq =
+    let getAfterShifts (start: coordinates<'Size>) (shifts: (shift<'Size>) seq) : coordinates<'Size>seq =
         Seq.map (getAfterShift start) shifts
     
     /// Get up to 8 unique coordinates that are a given metropolitan distance away from given coordinates
-    let getAfterShiftInAllDirections (start: coordinates<'Size>) ((i, j) : struct ('Size * 'Size)) : coordinates<'Size>seq =
+    let getAfterShiftInAllDirections (start: coordinates<'Size>) ((i, j) : shift<'Size>) : coordinates<'Size>seq =
         [
             struct (i,j);
             struct (i,-j);
@@ -67,7 +69,7 @@ module Coordinates =
         |> Seq.append (getOrthogonalAdjacentCoordinates start)
         
     /// Get all coordinates after repeating a shift, up to and including when a stopping function criteria is met.
-    let rec afterRepeatedShift (stopAt: coordinates<'Size> -> bool) (shift: struct ('Size * 'Size)) (start: coordinates<'Size>) : coordinates<'Size>list =
+    let rec afterRepeatedShift (stopAt: coordinates<'Size> -> bool) (shift: shift<'Size>) (start: coordinates<'Size>) : coordinates<'Size>list =
         let newCoordinates = getAfterShift shift start
         if stopAt newCoordinates then
             [newCoordinates]
