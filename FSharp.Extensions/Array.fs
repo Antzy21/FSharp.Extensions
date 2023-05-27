c6597c96b12d5960f00cb2050f72ab09e7a385ff
Anthony Dunford <trashzy21@gmail.com>
2023-05-27 10:30:02 +0100

    Copy List functions and make them for Arrays

---
+++ FSharp.Extensions/Array.fs
@@ -0,0 +1,11 @@
+﻿module FSharp.Extensions.Array
+
+let filterSome (source: 'T option array) : 'T array = 
+    source
+    |> Array.filter Option.isSome
+    |> Array.map Option.get
+
+let filterResults (source: 'T result array) : 'T array = 
+    source
+    |> Array.map Result.toOption
+    |> filterSome
